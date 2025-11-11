1b2092b1a8acf2299fd7c347bee5ad8e967bc35f
AnnBeny <anna.benyskova@gmail.com>
2025-11-11 11:44:04 +0100

    delete RUNs

--- RUN_CLL.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /mnt/hdd2/anna/LYNX/venv/bin/activate
-bash ../scripts/download_cns_CLL.sh &
-python ../scripts/merge_cna_CLL.py &
-bash ../scripts/download_snv_CLL.sh &
-python ../scripts/merge_snv_CLL.py &
-wait
--- RUN_DLBCL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /mnt/hdd2/anna/LYNX/venv/bin/activate
-bash ../scripts/download_cns_DLBCL.sh &
-python ../scripts/merge_cna_DLBCL.py &
-bash ../scripts/download_snv_DLBCL.sh &
-python ../scripts/merge_snv_DLBCL.py &
-python ../scripts/separate_rearrang_dlbcl.py &
-python ../scripts/separate_transl_dlbcl.py &
-wait
